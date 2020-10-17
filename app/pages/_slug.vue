a52426098d1ab01b3aa9b28983d562f0f151974d
iemong <kiyoshidainagon@gmail.com>
2020-10-17 11:29:01 +0900

    動的なページを追加

---
+++ app/pages/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="slug">
+    <p>_slug.vue</p>
+    <p>{{ params }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Slug',
+  asyncData({ params }) {
+    return {
+      params: JSON.stringify(params),
+    }
+  },
+}
+</script>
+
+<style scoped></style>
